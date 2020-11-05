770b5e269167929d73eaf7a1c65026df904b6ba5
Tpat21 <41355131+Tpat21@users.noreply.github.com>
2020-11-05 14:24:54 -0500

    Boarding Methods

---
+++ methods.py
@@ -0,0 +1,22 @@
+class Methods:
+
+    def backToFront:
+        pass
+
+    def zoneRoate:
+        pass
+
+    def reversePyramid:
+        pass
+
+    def efficientMethods:
+        pass
+
+    def optimalStrategy:
+        pass
+
+    def steffensOptimal:
+        pass
+
+    def outsideIn:
+        pass
